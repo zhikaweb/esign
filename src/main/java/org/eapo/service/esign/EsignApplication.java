9acdc4d307e306397a56964c98f09a0975afaed3
AlexandrStal <al_stal@mail.ru>
2020-10-29 15:23:21 +0300

    заработал конфиг ws

--- src/main/java/org/eapo/service/esign/EsignApplication.java
+++ src/main/java/org/eapo/service/esign/EsignApplication.java
@@ -6,12 +6,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 import java.security.Security;
 
+@EnableWs
 @SpringBootApplication
 @Configuration
 @ComponentScan(basePackages = {"org.eapo.service.esign.rest",
@@ -23,7 +33,7 @@ import java.security.Security;
         "org.eapo.service.esign.exception",
         "org.eapo.service.esign.ws"
 })
-public class EsignApplication {
+public class EsignApplication extends WsConfigurerAdapter {
 
     private static Logger logger = LoggerFactory.getLogger(EsignApplication.class.getName());
 
@@ -39,5 +49,28 @@ public class EsignApplication {
         Security.setProperty("crypto.policy", "unlimited");
         Security.addProvider(new BouncyCastleProvider());
         logger.debug("BouncyCastle is ready!");
+    }
+
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+
+    @Bean(name = "phoenix-upload")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema phoenixUploadSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("UploadPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://service.upload.phoenix.eapo.org");
+        wsdl11Definition.setSchema(phoenixUploadSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema uploadSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("phoenix-upload.xsd"));
     }
 }
