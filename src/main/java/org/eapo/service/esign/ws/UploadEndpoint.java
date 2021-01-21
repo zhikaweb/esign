ab784148c337ee3901b478e83ac234b341229490
AlexandrStal <al_stal@mail.ru>
2021-01-21 13:03:30 +0300

    Если сервис загрузки не отработал - выводим в лог

--- src/main/java/org/eapo/service/esign/ws/UploadEndpoint.java
+++ src/main/java/org/eapo/service/esign/ws/UploadEndpoint.java
@@ -3,6 +3,8 @@ package org.eapo.service.esign.ws;
 import org.eapo.phoenix.upload.service.Upload2PhoenixRequest;
 import org.eapo.phoenix.upload.service.Upload2PhoenixResponse;
 import org.eapo.service.esign.service.phoenix.PhoenixService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -14,6 +16,8 @@ import java.util.Date;
 
 @Endpoint
 public class UploadEndpoint {
+
+	private static Logger logger = LoggerFactory.getLogger(UploadEndpoint.class.getName());
 
 	private static final String NAMESPACE_URI = "http://service.upload.phoenix.eapo.org";
 
@@ -34,10 +38,12 @@ public class UploadEndpoint {
 			phoenixService.upload(request.getDosier(), request.getFile(), request.getDoccode(), dtsend);
 			response.setIsSaved(true);
 			response.setMessage("OK");
+			logger.info("File was uploaded for dossier {}", request.getDosier());
 		} catch (Exception e) {
-			e.printStackTrace();
 			response.setIsSaved(false);
 			response.setMessage(e.getMessage());
+			logger.error("File was NOT uploaded for dossier {}, exception is {}", request.getDosier(), e.getMessage());
+			logger.error("Uploading error", e);
 		}
 		return response;
 	}
